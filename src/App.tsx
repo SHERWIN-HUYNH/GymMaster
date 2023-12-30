f088465b923ff0fe16979c4f6d2d773a58f579f2
unknown <huynhchitrung020503@gmail.com>
2023-12-30 15:02:50 +0700

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
   }, []);
   return (
     <div className="app bg-gray-20">
+      {/* COMPONENTS HERE */}
       <Navbar
         isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}

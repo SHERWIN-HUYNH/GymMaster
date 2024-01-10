aecbc201543dd00f499c12f2ead26aba9ec3c331
unknown <huynhchitrung020503@gmail.com>
2024-01-10 16:27:50 +0700

    Add modules +  controller folder

---
+++ apps/api/modules/index.ts
@@ -0,0 +1,49 @@
+
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+
+export const config = {
+  runtime: "edge",
+};
+
+const app = new Hono().basePath("/api");
+
+app.use("*", logger());
+app.use(
+  "*",
+  cors({
+    origin: ["http://localhost:5173", "https://api-tau-seven-72.vercel.app"],
+    credentials: true,
+  })
+);
+app.get("/sign-in", (c) => {
+  return c.json({
+    message: "SIGN IN SUCCESSFULLY",
+  });
+});
+// app.post("/sign-in",async (c) =>{
+//   const res = await c.req.json();
+//   return res.json()
+// })
+
+app.get("/test", (c) => {
+  return c.json(
+   [ {
+    name: "hello SUCCESSFULLY",
+    id: 1,
+  },
+  {
+    name: "HUYNH",
+    id: 2,
+  }]
+  );
+});
+app.get("/hello", (c) => {
+  return c.json({
+    message: "hello SUCCESSFULLY",
+  });
+});
+
+export default handle(app);

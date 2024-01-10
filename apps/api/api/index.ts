1e363a87abfc087fce21922a786e1078dabf8e25
unknown <huynhchitrung020503@gmail.com>
2024-01-10 15:45:29 +0700

    add test path

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -13,12 +13,9 @@ app.use("*", logger());
 app.use(
   "*",
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://api-tau-seven-72.vercel.app",
-    ],
+    origin: ["http://localhost:5173", "https://api-tau-seven-72.vercel.app"],
     credentials: true,
-  }),
+  })
 );
 app.get("/sign-in", (c) => {
   return c.json({
@@ -30,21 +27,15 @@ app.get("/sign-in", (c) => {
 //   return res.json()
 // })
 
-app.get('test',(c) => {
-
-  return c.json({
-    message: "hello SUCCESSFULLY",
-  })
-});
-app.get("/hello", (c) => {
-
+app.get("/test", (c) => {
   return c.json({
     message: "hello SUCCESSFULLY",
   });
-
 });
-
-
-
+app.get("/hello", (c) => {
+  return c.json({
+    message: "hello SUCCESSFULLY",
+  });
+});
 
 export default handle(app);

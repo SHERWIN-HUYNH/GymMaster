f03ba88c243553d62994acd3b9198680d5d5d8be
unknown <huynhchitrung020503@gmail.com>
2024-01-11 15:49:20 +0700

    Change structures of app

---
+++ apps/server/src/app.ts
@@ -0,0 +1,47 @@
+
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import {router as auth} from '../src/modules/auth/auth.controller'
+export const config = {
+  runtime: "edge",
+};
+
+const app = new Hono().basePath("/api");
+
+app.use("*", logger());
+app.use(
+  "*",
+  cors({
+    origin: ["http://localhost:5173", "https://api-tau-seven-72.vercel.app"],
+    credentials: true,
+  })
+);
+// app.get("/sign-in", (c) => {
+//   return c.json({
+//     message: "SIGN IN SUCCESSFULLY",
+//   });
+// });
+
+// app.get("/test", (c) => {
+//   return c.json(
+//    [ {
+//     name: "hello SUCCESSFULLY",
+//     id: 1,
+//   },
+//   {
+//     name: "HUYNH",
+//     id: 2,
+//   }]
+//   );
+// });
+// app.get("/hello", (c) => {
+//   return c.json({
+//     message: "hello SUCCESSFULLY",
+//   });
+// });
+
+
+app.route('/',auth)
+export default app;
